2c22541a1c3d9be6d71b2851ec5682eabbbfee47
David Boyne <dave@deadhappy.com>
2020-11-30 07:32:01 +0000

    feature: added amplify

---
+++ src/icons/Arch_Developer- Tools/32/ArchAwsAmplify32.js
@@ -0,0 +1,28 @@
+import * as React from "react"
+
+function SvgArchAmplify32(props) {
+	return (
+		<svg width={40} height={40} xmlns="http://www.w3.org/2000/svg" {...props}>
+			<g>
+				<rect fill="#cd3f3c" id="canvas_background" height="44" width="44" y="-1" x="-1"/>
+				<g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
+					<rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>
+				</g>
+			</g>
+			<g>
+
+				<g stroke="null" id="svg_13">
+					<g stroke="null" transform="matrix(0.24427158490029297,0,0,0.262266745435647,0.3869015167067027,7.026955368797123) " fill-rule="evenodd" fill="none" id="svg_7">
+						<g stroke="null" id="svg_8">
+							<path stroke="null" fill="#ffffff" id="svg_10" d="m59.53671,37.84041l9.09583,15.76109l-17.72754,30.69616l35.44214,0l9.05828,15.69542l-71.74389,0l25.28086,-43.80786l10.59432,-18.34481zm35.85473,-31.84733l54.27009,94l-17.50228,0l-54.24809,-94l17.48028,0zm-22.34116,8.41854l49.42572,85.5812l-17.50358,0l-40.65548,-70.44849l8.73334,-15.13271z"/>
+							<polyline stroke="null" fill="#ffffff" points="23.661529541015625,99.99310255050659 95.66151809692383,99.99310255050659 86.57090377807617,83.99308729171753 51.00225067138672,83.99308729171753 " id="svg_9"/>
+						</g>
+					</g>
+				</g>
+				<desc transform="matrix(1.5006446266816056,0,0,1.5006446266816056,0,0) ">Created with Sketch.</desc>
+			</g>
+		</svg>
+	)
+}
+
+export default SvgArchAmplify32
